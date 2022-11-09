af3f38a6a8f801db669b3eebdf3b6701a377c5ab
TheBrex <marcobresciani.1998@gmail.com>
2022-11-09 16:50:27 +0100

    DATA VISUALIZATION

--- functions.py
+++ functions.py
@@ -1,2 +1,10 @@
+import pandas as pd
+
 def delete(variable):
     del variable
+
+def initialize():
+    X_train = pd.read_csv('dataset/train_data.csv')   
+    X_test = pd.read_csv('dataset/test_data.csv')
+    house_df = pd.read_csv('dataset/dataHouse.csv')
+    return X_train,X_test,house_df
