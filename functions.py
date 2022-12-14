655ea48e8eda53b9fcfa91c930333e28db44f8c6
TheBrex <marcobresciani.1998@gmail.com>
2022-12-14 18:46:44 +0100

    Refactoring Feature Engineering data_visualization

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import dill
 
 def delete(variable):
     del variable
@@ -8,3 +9,9 @@ def initialize():
     X_test = pd.read_csv('dataset/test_data.csv')
     house_df = pd.read_csv('dataset/dataHouse.csv')
     return X_train,X_test,house_df
+
+def save_session() :
+    dill.dump_session('notebook_env.db')
+
+def load_session():
+    dill.load_session('notebook_env.db')
